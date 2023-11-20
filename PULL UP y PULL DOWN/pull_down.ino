337cca3f240e8df620603e461d6da0890e3c5d8a
Nichokas <nicolas.rodalv@educa.jcyl.es>
2023-11-20 14:05:16 +0100

    Primer intento con for
    Con bucles for

--- PULL UP y PULL DOWN/pull_down.ino
+++ PULL UP y PULL DOWN/pull_down.ino
@@ -1,6 +1,18 @@
+int x=0;
+
 void setup(){
   pinMode(7, OUTPUT);
   pinMode(3, INPUT);
 }
 
-void loop(){}
+void loop(){
+  x = digitalRead(3);
+
+  for(x=1){
+    digitalWrite(7,1);
+  }
+
+  for(x=0){
+    digitalWrite(7,0);
+  }
+}
