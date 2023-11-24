bd94d113991d6e665db3149e17f1a75689be6f18
Nichokas <nicolas.rodalv@educa.jcyl.es>
2023-11-24 12:20:49 +0100

    Codigo del pull up funcionando

---
+++ PULL UP y PULL DOWN/pull_up.ino
@@ -0,0 +1,15 @@
+int x=0;
+
+void setup(){
+  pinMode(7, OUTPUT);
+  pinMode(3, INPUT);
+}
+
+void loop(){
+  if(digitalRead(3)==0){
+    digitalWrite(7,1);
+  }
+  else{
+    digitalWrite(7,0);
+  }
+}
