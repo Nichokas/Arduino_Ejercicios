2bd3088e417d0e881321431f6026e1a4f35e24af
Nichokas <nicolas.rodalv@educa.jcyl.es>
2023-11-27 14:05:05 +0100

    mas o menos funciona

--- Examen T1/code.ino
+++ Examen T1/code.ino
@@ -1,26 +1,28 @@
 int x=0;
+int y=0;
 
 void setup(){
-{
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(10, OUTPUT);
+
   pinMode(9, OUTPUT);
   pinMode(8, OUTPUT);
-  pinMode(3, INPUT);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(2, INPUT);
 }
 
 void loop(){
-  if(digitalRead(3)==1){
-    {
-    for(x=3;x<14;x=x+2){
+  if(digitalRead(2)==1){x=1}
+  if(digitalRead(2)==0){x=0}
+  if(x==1){
+    for(x=4;x<10;x++){
         digitalWrite(x,1);
-        delay(2000);
+        delay(350);
+        digitalWrite(x,0);
     }
     for(x=13;x>2;x=x-2){
         digitalWrite(x,0);
     }
-  }
-
+}
 }
