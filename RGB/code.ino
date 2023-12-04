917ed4584427c84fe76a80f6027ae1f735e21017
Nichokas <nicolas.rodalv@educa.jcyl.es>
2023-12-04 14:05:38 +0100

    funciona

--- RGB/code.ino
+++ RGB/code.ino
@@ -11,8 +11,45 @@ void setup()
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(3000);
+  
+  digitalWrite(R, 0);
+  digitalWrite(A, 0);
+  digitalWrite(V, 1);
+  
+  delay(3000);
+  
+  digitalWrite(R, 0);
+  digitalWrite(A, 1);
+  digitalWrite(V, 0);
+  
+  delay(3000);
+  
+  digitalWrite(R, 0);
+  digitalWrite(A, 0);
+  digitalWrite(V, 1);
+
+  delay(3000);
+
+  digitalWrite(R, 1);
+  digitalWrite(A, 1);
+  digitalWrite(V, 0);
+
+  delay(3000);
+
+  digitalWrite(R, 1);
+  digitalWrite(A, 0);
+  digitalWrite(V, 1);
+
+  delay(3000);
+
+  digitalWrite(R, 0);
+  digitalWrite(A, 1);
+  digitalWrite(V, 1);
+
+  delay(3000);
+
+  digitalWrite(R, 1);
+  digitalWrite(A, 1);
+  digitalWrite(V, 1);
 }
