380089d75329b6d9d7ca7d426d65c4f611c7e446
Nichokas <nicolas.rodalv@educa.jcyl.es>
2023-12-04 14:17:31 +0100

    echo

--- RGB/code.ino
+++ RGB/code.ino
@@ -7,49 +7,62 @@ void setup()
   pinMode(R, OUTPUT);
   pinMode(A, OUTPUT);
   pinMode(V, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  delay(3000);
-  
   digitalWrite(R, 0);
   digitalWrite(A, 0);
-  digitalWrite(V, 1);
+  digitalWrite(V, 0);
+  Serial.println("Apagado");
+
+  delay(3000);
+  
+  digitalWrite(R, 1);
+  digitalWrite(A, 0);
+  digitalWrite(V, 0);
+  Serial.println("Rojo");
   
   delay(3000);
   
   digitalWrite(R, 0);
   digitalWrite(A, 1);
   digitalWrite(V, 0);
+  Serial.println("Azul");
   
   delay(3000);
   
   digitalWrite(R, 0);
   digitalWrite(A, 0);
   digitalWrite(V, 1);
+  Serial.println("Verde");
 
   delay(3000);
 
   digitalWrite(R, 1);
   digitalWrite(A, 1);
   digitalWrite(V, 0);
+  Serial.println("Purpra");
 
   delay(3000);
 
   digitalWrite(R, 1);
   digitalWrite(A, 0);
   digitalWrite(V, 1);
+  Serial.println("Amarillo");
 
   delay(3000);
 
   digitalWrite(R, 0);
   digitalWrite(A, 1);
   digitalWrite(V, 1);
+  Serial.println("Cyan");
 
   delay(3000);
 
   digitalWrite(R, 1);
   digitalWrite(A, 1);
   digitalWrite(V, 1);
+  Serial.println("Blanco");
 }
