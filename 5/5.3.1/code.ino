38fb19676f39af88ea5d6765b7aa532e58088626
Nichokas <67232385+Nichokas@users.noreply.github.com>
2023-12-11 13:18:11 +0000

    Terminado 5.3.1

--- 5/5.3.1/code.ino
+++ 5/5.3.1/code.ino
@@ -7,53 +7,18 @@ void setup()
   pinMode(R, OUTPUT);
   pinMode(A, OUTPUT);
   pinMode(V, OUTPUT);
-  Serial.begin(9600);
 }
 
 void loop()
 {
-  digitalWrite(R, 0);
-  digitalWrite(A, 0);
-  digitalWrite(V, 0);
-
-  delay(3000);
-  
-  digitalWrite(R, 1);
-  digitalWrite(A, 0);
-  digitalWrite(V, 0);
-  
-  delay(3000);
-  
-  digitalWrite(R, 0);
-  digitalWrite(A, 1);
-  digitalWrite(V, 0);
-  
-  delay(3000);
-  
-  digitalWrite(R, 0);
-  digitalWrite(A, 0);
-  digitalWrite(V, 1);
-
-  delay(3000);
-
-  digitalWrite(R, 1);
-  digitalWrite(A, 1);
-  digitalWrite(V, 0);
-
-  delay(3000);
-
-  digitalWrite(R, 1);
-  digitalWrite(A, 0);
-  digitalWrite(V, 1);
-
-  delay(3000);
-
-  digitalWrite(R, 0);
-  digitalWrite(A, 1);
-  digitalWrite(V, 1);
-  delay(3000);
-
-  digitalWrite(R, 1);
-  digitalWrite(A, 1);
-  digitalWrite(V, 1);
+  for(int r = 0; r <= 1; r++) {
+    for(int a = 0; a <= 1; a++) {
+      for(int v = 0; v <= 1; v++) {
+        digitalWrite(R, r);
+        digitalWrite(A, a);
+        digitalWrite(V, v);
+        delay(3000);
+      }
+    }
+  }
 }
