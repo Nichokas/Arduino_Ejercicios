90825aea441ee9a1cbd3d50a4dfe0eba387a40a9
Nichokas <nicolas.rodalv@educa.jcyl.es>
2024-01-13 14:13:34 +0100

    ida y vuelta funcionan

--- display 7/ida y vuelta.ino
+++ display 7/ida y vuelta.ino
@@ -15,12 +15,18 @@ void setup()
   for(i=2; i<9;i=i+1){
     pinMode(i, OUTPUT);
   }
+  Serial.begin(9600);
 }
 
 void loop()
 {
   for(x=2; x<9;x=x+1){
     digitalWrite(x,1);
+    delay(333);
+  }
+  delay(22);
+  for(x=8; x>=2; x=x-1){
+    digitalWrite(x,0);
     delay(333);
   }
 }
