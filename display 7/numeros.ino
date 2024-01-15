26c66c0d7137f7134d23d5fec5721baa87c03f65
Nichokas <nicolas.rodalv@educa.jcyl.es>
2024-01-15 14:00:37 +0100

    4

--- display 7/numeros.ino
+++ display 7/numeros.ino
@@ -61,5 +61,13 @@ void loop()
   digitalWrite(G,1);
   digitalWrite(DP,0);
 
-
+//4
+  digitalWrite(A,0);
+  digitalWrite(B,1);
+  digitalWrite(C,1);
+  digitalWrite(D,0);
+  digitalWrite(E,0);
+  digitalWrite(F,1);
+  digitalWrite(G,1);
+  digitalWrite(DP,0);
 }
