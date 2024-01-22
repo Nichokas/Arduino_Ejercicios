e0708d4fcc8a60fcb71da61449a1b575f1188510
Nichokas <nicolas.rodalv@educa.jcyl.es>
2024-01-22 06:32:04 -0800

    listo para empezar

--- 6/6.3 OR.ino
+++ 6/6.3 OR.ino
@@ -1,2 +1,16 @@
 //Si no se pulsa ninguno = 0
 //si se pulsa uno o los 2 = 1
+
+int P1 = 10;
+int P2 = 12;
+
+void setup()
+{
+  pinMode(P1, INPUT);
+  pinMode(P2, INPUT);
+}
+
+void loop()
+{
+  
+}
