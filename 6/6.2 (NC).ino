7f76b72f9a18546f0a86200409f22f90032a5fd7
Nichokas <nicolas.rodalv@educa.jcyl.es>
2024-01-22 14:11:03 +0100

    NC

---
+++ 6/6.2 (NC).ino
@@ -0,0 +1,18 @@
+int LED = 8;
+int p = 12;
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+  pinMode(p, INPUT);
+}
+
+void loop()
+{
+ if (digitalRead(p)==1){
+    digitalWrite(LED, 1);
+ }
+ if (digitalRead(p)==0){
+  digitalWrite(LED, 0);
+ }
+}
