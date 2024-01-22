e9a5de9c2182f625505c0fe6621dd6416d81274b
Nichokas <nicolas.rodalv@educa.jcyl.es>
2024-01-22 09:56:03 -0800

    preparado para empezar

---
+++ 6/6.4 AND.ino
@@ -0,0 +1,15 @@
+//los dos presionados = 1
+//uno presionado o ninguno = 0
+int P1 = 10;
+int P2 = 12;
+
+void setup()
+{
+  pinMode(P1, INPUT);
+  pinMode(P2, INPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+    
+}
