0d38a385e459d64dee14324f35e81c250783d4c3
Nichokas <nicolas.rodalv@educa.jcyl.es>
2024-01-22 09:53:08 -0800

    OR echo

--- 6/6.3 OR.ino
+++ 6/6.3 OR.ino
@@ -8,16 +8,17 @@ void setup()
 {
   pinMode(P1, INPUT);
   pinMode(P2, INPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  if (digitalRead(P1)==1 or digitalRead(P2)==1){
-    Serial.println("1");
-  }
+  if (digitalRead(P1)==0 or digitalRead(P2)==0){
+      Serial.println("1");}
 
-  if (digitalRead(P2)==0 or digitalRead(P1)==0){
+  else{
+    if (digitalRead(P2)==1 or digitalRead(P1)==1){
     Serial.println("0");
+    }
   }
-
 }
