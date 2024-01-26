37a569ec1e0873431a478968069239700f3e040e
Nichokas <67232385+Nichokas@users.noreply.github.com>
2024-01-26 11:51:50 +0100

    Update 6.3 OR.ino

--- 6/6.3 OR.ino
+++ 6/6.3 OR.ino
@@ -3,22 +3,25 @@
 
 int P1 = 10;
 int P2 = 12;
+int LED = 8;
 
 void setup()
 {
   pinMode(P1, INPUT);
   pinMode(P2, INPUT);
+  pinMode(LED, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop()
 {
   if (digitalRead(P1)==0 or digitalRead(P2)==0){
-    Serial.println("1");}
+     Serial.println("1");
+  	 digitalWrite(LED, 1);}
 
   else{
     if (digitalRead(P2)==1 or digitalRead(P1)==1){
     Serial.println("0");
-    }
+    digitalWrite(LED, 0);}
   }
 }
