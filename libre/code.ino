a83289115d3779a285d89262f6c01012bc6c9b1a
Nichokas <nicolas.rodalv@educa.jcyl.es>
2024-02-05 14:20:14 +0100

    si

---
+++ libre/code.ino
@@ -0,0 +1,153 @@
+int A=3;
+int B=2;
+int C=6;
+int D=7;
+int E=8;
+int F=4;
+int G=5;
+int DP=9;
+
+int V=0;
+int R=1;
+
+int mas=10;
+int menos=11;
+int st=12;
+
+void setup()
+{
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
+  pinMode(F, OUTPUT);
+  pinMode(G, OUTPUT);
+  pinMode(DP, OUTPUT);
+
+  pinMode(V, OUTPUT);
+  pinMode(R, OUTPUT);
+
+  pinMode(mas, INPUT);
+  pinMode(menos, INPUT);
+  pinMode(st, INPUT);
+}
+
+void loop()
+{
+
+}
+
+void dp_controler(){
+    switch{
+            case 0:
+        digitalWrite(A,1);
+        digitalWrite(B,1);
+        digitalWrite(C,1);
+        digitalWrite(D,1);
+        digitalWrite(E,1);
+        digitalWrite(F,1);
+        digitalWrite(G,0);
+        digitalWrite(DP,0);
+         break;
+
+          case 1:
+        digitalWrite(A,0);
+        digitalWrite(B,1);
+        digitalWrite(C,1);
+        digitalWrite(D,0);
+        digitalWrite(E,0);
+        digitalWrite(F,0);
+        digitalWrite(G,0);
+        digitalWrite(DP,0);
+        break;
+
+          case 2:
+        digitalWrite(A,1);
+        digitalWrite(B,1);
+        digitalWrite(C,0);
+    digitalWrite(D,1);
+  digitalWrite(E,1);
+  digitalWrite(F,0);
+  digitalWrite(G,1);
+  digitalWrite(DP,0);
+  break;
+
+    case 3:
+  digitalWrite(A,1);
+  digitalWrite(B,1);
+  digitalWrite(C,1);
+  digitalWrite(D,1);
+  digitalWrite(E,0);
+  digitalWrite(F,0);
+  digitalWrite(G,1);
+  digitalWrite(DP,0);
+  break;
+
+    case 4:
+  digitalWrite(A,0);
+  digitalWrite(B,1);
+  digitalWrite(C,1);
+  digitalWrite(D,0);
+  digitalWrite(E,0);
+  digitalWrite(F,1);
+  digitalWrite(G,1);
+  digitalWrite(DP,0);
+  break;
+
+    case 5:
+  digitalWrite(A,1);
+  digitalWrite(B,0);
+  digitalWrite(C,1);
+  digitalWrite(D,1);
+  digitalWrite(E,0);
+  digitalWrite(F,1);
+  digitalWrite(G,1);
+  digitalWrite(DP,0);
+  break;
+
+    case 6:
+  digitalWrite(A,1);
+  digitalWrite(B,0);
+  digitalWrite(C,1);
+  digitalWrite(D,1);
+  digitalWrite(E,1);
+  digitalWrite(F,1);
+  digitalWrite(G,1);
+  digitalWrite(DP,0);
+  break;
+
+    case 7:
+  digitalWrite(A,1);
+  digitalWrite(B,1);
+  digitalWrite(C,1);
+  digitalWrite(D,0);
+  digitalWrite(E,0);
+  digitalWrite(F,0);
+  digitalWrite(G,0);
+  digitalWrite(DP,0);
+  break;
+
+    case 8:
+  digitalWrite(A,1);
+  digitalWrite(B,1);
+  digitalWrite(C,1);
+  digitalWrite(D,1);
+  digitalWrite(E,1);
+  digitalWrite(F,1);
+  digitalWrite(G,1);
+  digitalWrite(DP,0);
+  break;
+
+    case 9:
+  digitalWrite(A,1);
+  digitalWrite(B,1);
+  digitalWrite(C,1);
+  digitalWrite(D,0);
+  digitalWrite(E,0);
+  digitalWrite(F,1);
+  digitalWrite(G,1);
+  digitalWrite(DP,0);
+  break;
+    }
+}
